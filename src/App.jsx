5c207fa85c5c142d79bf8322781141b28f3f49d7
Teja1655 <devineniteja02@gmail.com>
2024-03-11 15:04:15 +0530

    Added local storage to store the  employee data

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UploadButton from "./Components/UploadButton";
 import UploadMasterData from "./Components/UploadMasterData";
 import SuccessPopup from "./Components/SuccessPopup";
 import EmployeeForm from "./Components/EmployeeForm";
 import EmployeeDataTable from "./Components/Employee DataTable";
-
-
+ 
 const App = () => {
   const [showUpload, setShowUpload] = useState(false);
   const [showSuccessPopup, setShowSuccessPopup] = useState(false);
   const [uploadedData, setUploadedData] = useState(null);
   const [employeeData, setEmployeeData] = useState([]);
-
+ 
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("employeeData"));
+    if (storedData) {
+      setEmployeeData(storedData);
+    }
+  }, []);
+ 
   const handleUploadButtonClick = () => {
     setShowUpload(true);
-    };
-
+  };
+ 
   const handleSuccessModalButtonClick = () => {
     setShowSuccessPopup(false);
     setShowUpload(false);
   };
-
+ 
   const handleDataUpload = (data) => {
-    if(Array.isArray(data)){
-    setUploadedData(data);
-    setShowSuccessPopup(true);
-    }else{
-      console.log("Invalid Data Format:Data is not an array");
+    if (Array.isArray(data)) {
+      setUploadedData(data);
+      setShowSuccessPopup(true);
+      const mergedData = [...employeeData, ...data];
+      setEmployeeData(mergedData);
+      localStorage.setItem("employeeData", JSON.stringify(mergedData));
+    } else {
+      console.log("Invalid Data Format: Data is not an array");
     }
   };
-
+ 
   const handleEmployeeFormSubmit = (employee) => {
-    setEmployeeData([...employeeData, employee]);
+    const newData = [...employeeData, employee];
+    setEmployeeData(newData);
+    localStorage.setItem("employeeData", JSON.stringify(newData));
   };
-
-
+ 
   return (
     <div className="App">
-      {!showUpload && !showSuccessPopup && <UploadButton onClick={handleUploadButtonClick} />}
+      {!showUpload && !showSuccessPopup && (
+        <UploadButton onClick={handleUploadButtonClick} />
+      )}
       {showUpload && !showSuccessPopup && (
-        <UploadMasterData onSuccessUpload={handleDataUpload} setShowUpload={setShowUpload} />
+        <UploadMasterData
+          onSuccessUpload={handleDataUpload}
+          setShowUpload={setShowUpload}
+        />
       )}
       {showSuccessPopup && (
         <SuccessPopup
@@ -50,12 +65,10 @@ const App = () => {
           onButtonClick={handleSuccessModalButtonClick}
         />
       )}
-       <EmployeeForm onSubmit={handleEmployeeFormSubmit} />
-      <EmployeeDataTable employees={employeeData} />
+      <EmployeeForm onSubmit={handleEmployeeFormSubmit} />
+      <EmployeeDataTable employees={[...employeeData, ...(uploadedData || [])]} />
     </div>
   );
 };
-
+ 
 export default App;
-
-
